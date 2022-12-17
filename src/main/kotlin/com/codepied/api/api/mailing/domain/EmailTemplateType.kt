e90d373c3043bb66ae040403396af4223c3f8b22
Aivyss <hklee6417@gmail.com>
2022-12-18 04:01:38 +0900

    CA-5 [API] AWS Email Template Entity

---
+++ src/main/kotlin/com/codepied/api/api/mailing/domain/EmailTemplateType.kt
@@ -0,0 +1,11 @@
+package com.codepied.api.api.mailing.domain
+
+/**
+ * Email Template Type
+ *
+ * @author Aivyss
+ * @since 2022/12/18
+ */
+enum class EmailTemplateType {
+    EMAIL_SIGNUP_AUTHORIZATION
+}
